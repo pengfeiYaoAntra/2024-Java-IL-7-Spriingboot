a2e304ce4e9f052fab7e928cd1baa2953b60a803
MatthewYao2000 <matthew.yao.2000@gmail.com>
2024-05-23 09:50:19 -0700

    Initial commit

---
+++ src/main/java/com/antran/demo/Il2024Java7SpringApplication.java
@@ -0,0 +1,13 @@
+package com.antran.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Il2024Java7SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Il2024Java7SpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antran/demo/Il2024Java7SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antran.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Il2024Java7SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
